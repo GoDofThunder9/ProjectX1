76abc91d7062bdf14eef03ec6eb8e951bb537e5f
Ashish Agrawal <ashishagrawal0181@gmail.com>
2025-01-11 00:40:07 +0530

    Update the cors policy

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-  origin:'http://localhost:5173', // Your frontend origin
+  origin:'http://65.0.199.218:5173', // Your frontend origin
   credentials:true, // Allow credentials (cookies, authorization headers, etc.)
 }));
 
