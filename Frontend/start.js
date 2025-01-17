64b296775017bf649c29246d706a1d624b627bda
GoDofThunder9 <123013319+GoDofThunder9@users.noreply.github.com>
2025-01-17 14:56:01 +0530

    done

--- Frontend/start.js
+++
@@ -1,9 +0,0 @@
-// start.js
-import serve from 'serve';
-
-const server = serve('./Build', {
-  port: 5173,  // Port where the server will run
-  open: true,  // Optional: open the browser automatically
-});
-
-console.log('Serving React build on port 5173');
